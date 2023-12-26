6ed7822c7a289f0f4aa6f2845f5639239b797251
Yibsl <149065359+Yibsl@users.noreply.github.com>
2023-12-26 22:13:54 +0100

    back zur ofline db

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://donerdata_user:VSyl5Kog1COnS4qS8oXrV14igMODW7Jv@dpg-cm5jts0cmk4c73cqfh60-a/donerdata'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
